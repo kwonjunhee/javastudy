99cddd5b6375da5a6005e984dff1db723e2116ca
BIT <BIT@DESKTOP-BM40P2C>
2022-01-14 21:20:08 +0900

    prob05

--- practice02/src/main/java/prob5/Prob05.java
+++ practice02/src/main/java/prob5/Prob05.java
@@ -10,13 +10,23 @@ public class Prob05 {
 
 		while( true ) {
 			
-			/* 게임 작성 */
-
+			
 			// 정답 램덤하게 만들기
 			Random random = new Random();
 			int correctNumber = random.nextInt( 100 ) + 1;
-			System.out.println(correctNumber);
-
+			System.out.println("수를 결정하였습니다. 맞추어 보세요.");
+			int num = scanner.nextInt();
+			boolean flag = true;
+			while(flag) {
+				if(num>correctNumber) {
+					System.out.println("더 낮게");
+				} else if (num < correctNumber) {
+					System.out.println("더 높게");
+				} else {
+					System.out.println("맞았습니다");
+					flag=false;
+				}
+			}
 			
 			//새 게임 여부 확인하기
 			System.out.print( "다시 하겠습니까(y/n)>>" );
