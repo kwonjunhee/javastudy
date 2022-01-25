a1a50605e138d25d9d55b24e968f4dbdc8673f72
BIT <BIT@DESKTOP-BM40P2C>
2022-01-25 11:47:01 +0900

    dwqdw

---
+++ chapter03/src/main/java/exception/MyClassTest.java
@@ -0,0 +1,12 @@
+package exception;
+
+public class MyClassTest {
+	public static void main(String[] args) {
+		try {
+			MyClass mc = new MyClass();
+			mc.danger();
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+	}
+}
